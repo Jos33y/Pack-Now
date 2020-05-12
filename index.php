74913f3792ca6d6602cf5d700e58e66a17eb8c23
Jos33y <josephlagbalu@gmail.com>
2020-05-12 01:23:43 +0100

    created the header index page

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+$title = 'Home';
+include('header.html'); 
+
+?>
+<!--Body Tag-->
+<div class="container">
+    <div class="row">
+    <div class="col-md-6 text-center">
+            <img class="home-image" src="assets/img/boxes.jpg" alt="">
+        </div>
+        <div class="col-md-6">
+            <div class="style-text">
+            <h1 class="header-text">Create custom packaging</h1>
+            <p class="sub-text">Custom packaging can turn your brand into the total package with full
+                customization, instant quoting, and fast turnarounds.</p>
+            <button class="btn btn-lg btn-success">Choose your style</button>
+        </div>
+        </div>
+        
+    </div>
+</div>
+
+
+</div>
+
+<?php include('footer.php'); ?>
