1863c64964169efb8edba43ec7aa6254a9345599
Jos33y <josephlagbalu@gmail.com>
2020-05-12 20:41:59 +0100

    completed the home-page

--- index.php
+++ index.php
@@ -6,22 +6,241 @@ include('header.html');
 <!--Body Tag-->
 <div class="container">
     <div class="row">
-    <div class="col-md-6 text-center">
-            <img class="home-image" src="assets/img/boxes.jpg" alt="">
+        <div class="col-md-6 text-center">
+            <img class="home-image" src="assets/img/boxes.png" alt="">
         </div>
         <div class="col-md-6">
             <div class="style-text">
-            <h1 class="header-text">Create custom packaging</h1>
-            <p class="sub-text">Custom packaging can turn your brand into the total package with full
-                customization, instant quoting, and fast turnarounds.</p>
-            <button class="btn btn-lg btn-success">Choose your style</button>
+                <h1 class="header-text">Create custom packaging</h1>
+                <p class="sub-text">Custom packaging can turn your brand into the total package with full
+                    customization, instant quoting, and fast turnarounds.</p>
+                <button class="btn btn-lg btn-success">Choose your style</button>
+            </div>
         </div>
+
+    </div>
+</div>
+
+</div>
+<!--First Home Page Columns-->
+<div class="container card-col">
+
+    <h1 class="text-center first-head">Choose your custom box style</h1>
+    <hr width="30%" class="line">
+    <div class="row justify-content-center">
+        <div class="col-md-3">
+            <div class="card">
+                <img src="assets/img/image-one.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Custom Box</h5>
+                    <a style="text-decoration:none;" href="#">
+                        <p class="card-text">view more...</p>
+                    </a>
+                    <a href="#" class="btn btn-primary">Meet our designer</a>
+                </div>
+            </div>
         </div>
-        
+
+        <div class="col-md-3">
+            <div class="card">
+                <img src="assets/img/image-two.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Satin Bag </h5>
+                    <a style="text-decoration:none;" href="#">
+                        <p class="card-text">view more...</p>
+                    </a>
+                    <a href="#" class="btn btn-primary">Meet our designer</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card">
+                <img src="assets/img/image-three.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Paper Bag </h5>
+                    <a style="text-decoration:none;" href="#">
+                        <p class="card-text">view more...</p>
+                    </a>
+                    <a href="#" class="btn btn-primary">Meet our designers</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card">
+                <img src="assets/img/image-four.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Gift Box</h5>
+                    <a style="text-decoration:none;" href="#">
+                        <p class="card-text">view more...</p>
+                    </a>
+                    <a href="#" class="btn btn-primary">Meet our designers</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--Second Column of home page-->
+<div class="icons">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-md-4">
+                <i class="fas fa-infinity"></i>
+                <h2 class="icon-head">Tiny minimums. No maximums.</h2>
+                <p class="icon-text">No matter the size of your order, you'll get the best boxes at prices to match.
+                </p>
+            </div>
+            <div class="col-md-4">
+                <i class="fas fa-edit"></i>
+                <h2 class="icon-head">Instant quotes</h2>
+                <p class="icon-text"> Pick your style, design your custom boxes, and get an instant quote. </p>
+
+            </div>
+            <div class="col-md-4">
+                <i class="fas fa-truck"></i>
+                <h2 class="icon-head">Fast turnarounds</h2>
+                <p class="icon-text">Orders ship within 10 days or less, and even faster with rush delivery. </p>
+
+            </div>
+        </div>
     </div>
 </div>
 
 
+<!--Third Column of Home page Inspire Pictures -->
+<div class="home-inspire">
+    <div class="container text-center">
+        <div class="col-md-8 mx-auto">
+            <h1 class="first-head">Get inspired</h1>
+            <p class="icon-text"> We’re here to help. Whether you’re designing custom gift boxes with your logo
+                or need help with your box design, there’s lots of inspiration to be found for your custom product
+                packaging.
+            </p>
+        </div>
+        <div class="row home-inspire-row">
+            <div class="col-md-3">
+                <img src="assets/img/box-one.png" alt="" class="card-img-top">
+            </div>
+
+            <div class="col-md-3">
+                <img src="assets/img/box-two.png" alt="" class="card-img-top">
+            </div>
+
+            <div class="col-md-3">
+                <img src="assets/img/box-three.png" alt="" class="card-img-top">
+            </div>
+
+            <div class="col-md-3">
+                <img src="assets/img/box-four.png" class="card-img-top" alt="">
+            </div>
+
+        </div>
+    </div>
 </div>
 
-<?php include('footer.php'); ?>
+
+<!-- Fourth Column of More About our Package column leading to about section -->
+<div class="package">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="assets/img/gift-boxes.png" alt="">
+            </div>
+            <div class="col-md-5" style="margin-left:3%;">
+                <h1 class="first-head">More than just a pretty package </h1> <br>
+                <p class="icon-text"> Every custom box we create is made just for you. You can customize every inch of
+                    your
+                    box
+                    design inside and out, while our state of the art printing ensures you'll love the quality of
+                    your finished packaging boxes.
+                </p>
+
+                <a href="#" class="btn btn-primary">Find out what makes our boxes special</a>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!--Frequently Asked Questions-->
+<div class="faq">
+    <div class="container">
+            <div class="col-md-8 mx-auto text-center" style="margin-bottom: 5%">
+                <h1 class="first-head" style="margin-bottom:3%">Frequently Asked Questions</h1>
+                <p class="icon-text">
+                    Below you'll find answers to some common questions around creating a custom box. Every order is little
+                    different though,
+                    so don't hesitate to <a style="text-decoration: none;" href="#">reach out </a> with anything else you might
+                    be wondering.
+                </p>
+            </div>
+
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <h3 class="faq-head">Can you print inside the box?</h3>
+                <p class="faq-text">
+                    Yes! We can print on the inside of any of our corrugated box styles.
+                    This includes Mailers, Shippers, and Tuck Tops (our secret menu box style!).
+                    Folding cartons are currently limited to printing on the exterior only.
+                    You can even use our online box designer to design the inside of Mailer boxes.
+                    For Shippers and Tuck Tops, we currently require a 2D dieline template be submitted
+                    for both the interior and exterior so we can set up your order as a custom order.
+                    You can also submit designs this way for our Mailer boxes, if you prefer to build on a 2D
+                    Illustrator template instead of our free 3D box design tool.
+                </p>
+            </div>
+
+            <div class="col-md-6">
+                <h3 class="faq-head">What choices affect my pricing?</h3>
+                <p class="faq-text">
+                    As a high-volume producer with scale economies, Packlane provides the industry's most competitive
+                    prices on custom printed boxes available. Pricing is generally a factor of five things: dimensions,
+                    box style, ink coverage on the box, box material, and quantity (higher quantities = bulk quantity
+                    savings). If you have questions about pricing or choices that can affect your order, our customer
+                    support team is happy to help!
+
+                </p>
+            </div>
+
+            <div class="col-md-6">
+                <h3 class="faq-head">What is the turnaround time on my order?</h3>
+                <p class="faq-text">
+                    Our Standard turnaround for most custom box orders is around 10 business days and Rush turnaround is
+                    7 business days. These turnarounds do not include transit time (1 - 7 business days depending on
+                    your location). Bulk or special custom orders may require a few extra days to get setup and
+                    produced. Please note that during extremely busy times of year some orders may take longer to
+                    complete. While delivery estimates cannot be guaranteed, all dates shown at checkout are the
+                    quickest estimate we offer. If your delivery date is time sensitive, we highly recommend choosing
+                    Rush production and expedited shipping.
+                </p>
+            </div>
+
+            <div class="col-md-6">
+                <h3 class="faq-head">Will I see a proof for my order?</h3>
+                <p class="faq-text">
+                    All new orders will receive a digital proof within 24 hours of completing the checkout process.
+                </p>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+
+<!--Be A Designer column-->
+<div class="designers">
+    <div class="container text-center">
+        <div class="col-md-8 mx-auto">
+            <h1 class="designer-head">Join Packnow</h1>
+            <p class="icon-text">Join our team of designers and design customized boxes</p>
+            <a href="#" class="btn btn-primary">Become a Packnow Designer</a>
+        </div>
+    </div>
+</div>
+
+
+
+<?php include('footer.html'); ?>
